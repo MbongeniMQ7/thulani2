477c460cb9710d99555261b91d390f14589abd10
Mbongeni Qwabe <qwabembongeni074@gmail.com>
2025-10-27 18:34:25 +0200

    Exclude supabase functions from metro bundler

--- metro.config.js
+++ metro.config.js
@@ -2,4 +2,10 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
 
+// Exclude TypeScript files in supabase/functions from bundling
+config.resolver.blockList = [
+  /supabase\/functions\/.*/,
+  /functions\/.*/,
+];
+
 module.exports = config;
