c4991ddda2492362510169c9bf3d4a120356d0d3
Mbongeni Qwabe <qwabembongeni074@gmail.com>
2025-10-28 00:03:07 +0200

    Revert to simpler config - remove shims and scripts

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,5 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
-const path = require('path');
 const config = getDefaultConfig(__dirname);
 
 // Exclude TypeScript files in supabase/functions from bundling
@@ -8,11 +7,5 @@ config.resolver.blockList = [
   /supabase\/functions\/.*/,
   /functions\/.*/,
 ];
-
-// Provide shims for semver subpath imports used by react-native-reanimated
-// This maps imports like 'semver/functions/satisfies' to local shim files included in the repo
-config.resolver.extraNodeModules = config.resolver.extraNodeModules || {};
-config.resolver.extraNodeModules['semver/functions/satisfies'] = path.resolve(__dirname, 'shims/semver/functions/satisfies.js');
-config.resolver.extraNodeModules['semver/functions/prerelease'] = path.resolve(__dirname, 'shims/semver/functions/prerelease.js');
 
 module.exports = config;
